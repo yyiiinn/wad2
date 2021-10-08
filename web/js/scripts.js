fccc3366dc450cd4135be3422e1e5b5305a91945
yyiiinn <33303522+yyiiinn@users.noreply.github.com>
2021-10-08 15:30:30 +0800

    fix navbar at the top

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -23,12 +23,15 @@
 	
 	/* Navbar Scripts */
 	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
+    // $(window).on('scroll load', function() {
+	// 	if ($(".navbar").offset().top > 20) {
+	// 		$(".fixed-top").addClass("top-nav-collapse");
+	// 	} else {
+	// 		$(".fixed-top").removeClass("top-nav-collapse");
+	// 	}
+    // });
+    $(window).on('load', function() {
+		$(".fixed-top").addClass("top-nav-collapse");
     });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
