808537b72917f4fa6f251830ac5a7a744ca6958a
huisinnnn <huisin.lim.2018@smu.edu.sg>
2021-10-13 21:50:02 +0800

    Added firebase authentication

---
+++ web/js/database.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDdAd22NyJbxcmvuXV7ZK8CveZxcNFhSlA",
+    authDomain: "is216-wad2-countonme.firebaseapp.com",
+    projectId: "is216-wad2-countonme",
+    storageBucket: "is216-wad2-countonme.appspot.com",
+    messagingSenderId: "809642846340",
+    appId: "1:809642846340:web:0954681e97a149ff0f7ad1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
