bca22048fa1fb9cb7dec90d7b53db7662fbd8c8e
huisinnnn <huisin.lim.2018@smu.edu.sg>
2021-10-17 19:02:05 +0800

    Remove unnecessary codes from database.js

--- web/js/database.js
+++ web/js/database.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyDdAd22NyJbxcmvuXV7ZK8CveZxcNFhSlA",
     authDomain: "is216-wad2-countonme.firebaseapp.com",
     projectId: "is216-wad2-countonme",
@@ -12,6 +7,3 @@ const firebaseConfig = {
     messagingSenderId: "809642846340",
     appId: "1:809642846340:web:0954681e97a149ff0f7ad1"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
