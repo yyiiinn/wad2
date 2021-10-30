3377bb65c2613b4be29114482a51ea8b28e4f913
yyiiinn <33303522+yyiiinn@users.noreply.github.com>
2021-10-31 00:09:12 +0800

    finish up linking charts in view my diary with firebase

--- web/js/diaryStats.js
+++ web/js/diaryStats.js
@@ -29,7 +29,8 @@ $(function () {
         posResult = []; 
         negResult = [];
         NeuResult = [];
-        moodArray = []
+        moodArray = [];
+        sleepArray = new Array(24).fill(0);
         for(r of result){
             moodArray.push(r.mood)
             if (r.feeling == "Positive"){
@@ -41,11 +42,92 @@ $(function () {
             else if(r.feeling == "Neutral"){
                 NeuResult.push(r)
             }
+            hoursSlept = r.hoursSlept
+            sleepArray[hoursSlept] = sleepArray[hoursSlept]+1
         }
+        console.log(sleepArray)
+        var veryStress = result.reduce(function (n, r){
+            return n + (r.stress == "Very Stress");
+        }, 0);
+        var relaxed = result.reduce(function (n, r){
+            return n + (r.stress == "Relaxed");
+        }, 0); 
+        var feelingGood = result.reduce(function (n, r){
+            return n + (r.stress == "Feeling Good");
+        }, 0); 
+        var littleStress = result.reduce(function (n, r){
+            return n + (r.stress == "A Little Stress");
+        }, 0); 
+        var stress = result.reduce(function (n, r){
+            return n + (r.stress == "Stress");
+        }, 0); 
+        var calm = result.reduce(function (n, r){
+            return n + (r.anxiety == "Calm");
+        }, 0); 
+        var neutral = result.reduce(function (n, r){
+            return n + (r.anxiety == "Neutral");
+        }, 0); 
+        var cross = result.reduce(function (n, r){
+            return n + (r.anxiety == "Cross");
+        }, 0); 
+        var angry = result.reduce(function (n, r){
+            return n + (r.anxiety == "Angry");
+        }, 0); 
+        var furious = result.reduce(function (n, r){
+            return n + (r.anxiety == "Furious");
+        }, 0); 
+        stressSeries = [
+            {
+                data: [veryStress],
+                name: "Very Stress",
+                color: "darkblue",
+            },{
+                data: [stress],
+                name: 'Stress',
+                color:"#4f88d1",
+            }, {
+                data: [littleStress],
+                name: 'A little Stress',
+                color:"#6ec8ff",
+            }, {
+              data: [feelingGood],
+              name: 'Feeling Good',
+              color:"#a6ddff",
+          }, {
+            data: [relaxed],
+            name: 'Relaxed',
+            color:"#bae8ff",
+          }
+        ]
+        anxietySeries = [
+            {
+                data: [furious],
+                name: "Furious",
+                color: "darkred",
+            },{
+                data: [angry],
+                name: 'Angry',
+                color:"#da413d",
+            }, {
+                data: [cross],
+                name: 'Cross',
+                color:"#ff8575",
+            }, {
+              data: [neutral],
+              name: 'Neutral',
+              color:"#ffbab0",
+          }, {
+            data: [calm],
+            name: 'Calm',
+            color:"#f7d2cd",
+          }
+        ]
         positiveChart(posResult, result.length)
         negativeChart(negResult, result.length)
         neutralChart(NeuResult, result.length)
         callMonkeyLearn(moodArray)
+        createTracker(stressSeries, anxietySeries, result.length) 
+        sleepTracker(sleepArray, result.length)
 })
 });
 
@@ -306,247 +388,272 @@ function wordCloud(feelings, color){
 })
 }
 
-
-$(function(){
-  Highcharts.seriesType(
-    'pictorial',
-    'column',
-    {
-        borderWidth: 0
-    },
-    {
-        pointAttribs: function (point, selected) {
-            const pointAttribs = Highcharts.seriesTypes.column.prototype
-                .pointAttribs.call(this, point, selected);
-
-            pointAttribs.fill = {
-                pattern: {
-                    path: {
-                        d: point.series.options.paths[
-                            point.index % point.series.options.paths.length
-                        ],
-                        fill: pointAttribs.fill,
-                        strokeWidth: 0,
-                        stroke: pointAttribs.stroke
-                    },
-                    x: point.shapeArgs.x,
-                    y: 0,
-                    width: point.shapeArgs.width,
-                    height: point.series.yAxis.len,
-                    patternContentUnits: 'objectBoundingBox',
-                    backgroundColor: 'none'
-                }
-            };
-
-            delete pointAttribs.stroke;
-            delete pointAttribs.strokeWidth;
-
-            return pointAttribs;
+function createTracker(stressSeries, anxietySeries, totalDays){
+    Highcharts.seriesType(
+        'pictorial',
+        'column',
+        {
+            borderWidth: 0
+        },
+        {
+            pointAttribs: function (point, selected) {
+                const pointAttribs = Highcharts.seriesTypes.column.prototype
+                    .pointAttribs.call(this, point, selected);
+    
+                pointAttribs.fill = {
+                    pattern: {
+                        path: {
+                            d: point.series.options.paths[
+                                point.index % point.series.options.paths.length
+                            ],
+                            fill: pointAttribs.fill,
+                            strokeWidth: 0,
+                            stroke: pointAttribs.stroke
+                        },
+                        x: point.shapeArgs.x,
+                        y: 0,
+                        width: point.shapeArgs.width,
+                        height: point.series.yAxis.len,
+                        patternContentUnits: 'objectBoundingBox',
+                        backgroundColor: 'none'
+                    }
+                };
+    
+                delete pointAttribs.stroke;
+                delete pointAttribs.strokeWidth;
+    
+                return pointAttribs;
+            }
         }
-    }
-);
-
-Highcharts.addEvent(Highcharts.Series, 'afterRender', function () {
-  if (this instanceof Highcharts.seriesTypes.pictorial) {
-      this.points.forEach(point => {
-          const fill = point.graphic && point.graphic.attr('fill');
-          const match = fill && fill.match(/url\(([^)]+)\)/);
-          if (match) {
-              const patternPath = document.querySelector(`${match[1]} path`);
-              if (patternPath) {
-                  const bBox = patternPath.getBBox();
-                  const scaleX = 1 / bBox.width;
-                  const scaleY = this.yAxis.len /
-                      point.shapeArgs.height /
-                      bBox.height;
-
-                  patternPath.setAttribute(
-                      'transform',
-                      `scale(${scaleX} ${scaleY}) ` +
-                      `translate(${-bBox.x}, ${-bBox.y})`
-                  );
-              }
-          }
-
-      });
-  }
-});
-
-Highcharts.chart('stressTracker', {
-  chart: {
-      type: 'pictorial',
-  },
-  exporting: {
-    enabled: false
-  },credits: {
-     enabled: false
-  },
-  title: {
-      text: ''
-  },
-  xAxis: {
-      categories: [''],
-      lineWidth: 0
-  },
-  yAxis: {
-      visible: false
-  },
-  plotOptions: {
-      pictorial: {
-          stacking: 'percent',
-
-          /*
-          Icon paths from font-awesome, https://fontawesome.com/license,
-          translated using http://jsfiddle.net/highcharts/a0jrvxch/
-          */
-          paths: [
-              'M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z',
-              'M128 0c35.346 0 64 28.654 64 64s-28.654 64-64 64c-35.346 0-64-28.654-64-64S92.654 0 128 0m119.283 354.179l-48-192A24 24 0 0 0 176 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H80a24 24 0 0 0-23.283 18.179l-48 192C4.935 369.305 16.383 384 32 384h56v104c0 13.255 10.745 24 24 24h32c13.255 0 24-10.745 24-24V384h56c15.591 0 27.071-14.671 23.283-29.821z'
-          ],
-          pointPadding: 0
-      },
+    );
     
-allowPointSelect: false,
-  },
-  legend: {
-      align: 'center',
-      verticalAlign: 'middle',
-      layout: 'vertical'
-  }, 
-  series: [{
-      data: [10],
-      name: "Very Stress",
-      color:"darkblue"
-  }, {
-      data: [20],
-      name: 'Stress',
-      color:"#4f88d1"
-  }, {
-      data: [30],
-      name: 'A little Stress',
-      color:"#6ec8ff"
-  }, {
-    data: [30],
-    name: 'Feeling Good',
-    color:"#a6ddff"
-}, {
-  data: [30],
-  name: 'Relaxed',
-  color:"#bae8ff"
- 
-}],
-  
-  responsive: {
-      rules: [{
-          condition: {
-          },
-          chartOptions: {
-              legend: {
-                layout: 'horizontal',
-                align: 'center',
-                verticalAlign: 'top',
-              },
-              chart:{
-              }
-          }
-      }]
-
-  },
-  tooltip: {
-    formatter: function() {
-      var tooltip = '<br><span style="color:' + this.series.color + '">' + this.series.name + '</span>: ' + this.series.data[0].y + ' out of 300 days (50%)';
-      return tooltip;
-    } 
-  }
-});
-
-Highcharts.chart('anxietyTracker', {
-  chart: {
-      type: 'pictorial',
-  },
-  exporting: {
-    enabled: false
-  },credits: {
-     enabled: false
-  },
-  title: {
-      text: ''
-  },
-  xAxis: {
-      categories: [''],
-      lineWidth: 0
-  },
-  yAxis: {
-      visible: false
-  },
-  plotOptions: {
-      pictorial: {
-          stacking: 'percent',
-
-          /*
-          Icon paths from font-awesome, https://fontawesome.com/license,
-          translated using http://jsfiddle.net/highcharts/a0jrvxch/
-          */
-          paths: [
-              'M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z',
-              'M128 0c35.346 0 64 28.654 64 64s-28.654 64-64 64c-35.346 0-64-28.654-64-64S92.654 0 128 0m119.283 354.179l-48-192A24 24 0 0 0 176 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H80a24 24 0 0 0-23.283 18.179l-48 192C4.935 369.305 16.383 384 32 384h56v104c0 13.255 10.745 24 24 24h32c13.255 0 24-10.745 24-24V384h56c15.591 0 27.071-14.671 23.283-29.821z'
-          ],
-          pointPadding: 0
-      },
+    Highcharts.addEvent(Highcharts.Series, 'afterRender', function () {
+      if (this instanceof Highcharts.seriesTypes.pictorial) {
+          this.points.forEach(point => {
+              const fill = point.graphic && point.graphic.attr('fill');
+              const match = fill && fill.match(/url\(([^)]+)\)/);
+              if (match) {
+                  const patternPath = document.querySelector(`${match[1]} path`);
+                  if (patternPath) {
+                      const bBox = patternPath.getBBox();
+                      const scaleX = 1 / bBox.width;
+                      const scaleY = this.yAxis.len /
+                          point.shapeArgs.height /
+                          bBox.height;
     
-allowPointSelect: false,
-  },
-  legend: {
-      align: 'center',
-      verticalAlign: 'middle',
-      layout: 'vertical'
-  }, 
-  series: [{
-      data: [10],
-      name: "Furious",
-      color:"darkred"
-  }, {
-      data: [20],
-      name: 'Angry',
-      color:"#da413d"
-  }, {
-      data: [30],
-      name: 'Cross',
-      color:"#ff8575"
-  }, {
-    data: [30],
-    name: 'Neutral',
-    color:"#ffbab0"
-}, {
-  data: [30],
-  name: 'Calm',
-  color:"#f7d2cd"
- 
-}],
- 
-  responsive: {
-      rules: [{
-          condition: {
-          },
-          chartOptions: {
-              legend: {
-                layout: 'horizontal',
-                align: 'center',
-                verticalAlign: 'top',
-              },
-              chart:{
+                      patternPath.setAttribute(
+                          'transform',
+                          `scale(${scaleX} ${scaleY}) ` +
+                          `translate(${-bBox.x}, ${-bBox.y})`
+                      );
+                  }
               }
-          }
-      }]
+    
+          });
+      }
+    });
+    
+    Highcharts.chart('stressTracker', {
+      chart: {
+          type: 'pictorial',
+      },
+      exporting: {
+        enabled: false
+      },credits: {
+         enabled: false
+      },
+      title: {
+          text: ''
+      },
+      xAxis: {
+          categories: [''],
+          lineWidth: 0
+      },
+      yAxis: {
+          visible: false
+      },
+      plotOptions: {
+          pictorial: {
+              stacking: 'percent',
+    
+              /*
+              Icon paths from font-awesome, https://fontawesome.com/license,
+              translated using http://jsfiddle.net/highcharts/a0jrvxch/
+              */
+              paths: [
+                  'M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z',
+                  'M128 0c35.346 0 64 28.654 64 64s-28.654 64-64 64c-35.346 0-64-28.654-64-64S92.654 0 128 0m119.283 354.179l-48-192A24 24 0 0 0 176 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H80a24 24 0 0 0-23.283 18.179l-48 192C4.935 369.305 16.383 384 32 384h56v104c0 13.255 10.745 24 24 24h32c13.255 0 24-10.745 24-24V384h56c15.591 0 27.071-14.671 23.283-29.821z'
+              ],
+              pointPadding: 0
+          },
+        
+    allowPointSelect: false,
+      },
+      legend: {
+          align: 'center',
+          verticalAlign: 'middle',
+          layout: 'vertical'
+      }, 
+      series: stressSeries,
+      responsive: {
+          rules: [{
+              condition: {
+              },
+              chartOptions: {
+                  legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'top',
+                  },
+                  chart:{
+                  }
+              }
+          }]
+    
+      },
+      tooltip: {
+        formatter: function() {
+          percent = Math.round(this.series.data[0].y/totalDays * 100)
+          var tooltip = '<br><span style="color:' + this.series.color + '">' + this.series.name + '</span>: ' + this.series.data[0].y + ' out of ' + totalDays + ' days (' + percent + '%)';
+          return tooltip;
+        } 
+      }
+    });
+    
+    Highcharts.chart('anxietyTracker', {
+      chart: {
+          type: 'pictorial',
+      },
+      exporting: {
+        enabled: false
+      },credits: {
+         enabled: false
+      },
+      title: {
+          text: ''
+      },
+      xAxis: {
+          categories: [''],
+          lineWidth: 0
+      },
+      yAxis: {
+          visible: false
+      },
+      plotOptions: {
+          pictorial: {
+              stacking: 'percent',
+    
+              /*
+              Icon paths from font-awesome, https://fontawesome.com/license,
+              translated using http://jsfiddle.net/highcharts/a0jrvxch/
+              */
+              paths: [
+                  'M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z',
+                  'M128 0c35.346 0 64 28.654 64 64s-28.654 64-64 64c-35.346 0-64-28.654-64-64S92.654 0 128 0m119.283 354.179l-48-192A24 24 0 0 0 176 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H80a24 24 0 0 0-23.283 18.179l-48 192C4.935 369.305 16.383 384 32 384h56v104c0 13.255 10.745 24 24 24h32c13.255 0 24-10.745 24-24V384h56c15.591 0 27.071-14.671 23.283-29.821z'
+              ],
+              pointPadding: 0
+          },
+        
+    allowPointSelect: false,
+      },
+      legend: {
+          align: 'center',
+          verticalAlign: 'middle',
+          layout: 'vertical'
+      }, 
+      series: anxietySeries,
+      responsive: {
+          rules: [{
+              condition: {
+              },
+              chartOptions: {
+                  legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'top',
+                  },
+                  chart:{
+                  }
+              }
+          }]
+    
+      },
+      tooltip: {
+        formatter: function() {
+          percent = Math.round(this.series.data[0].y/totalDays * 100)
+          var tooltip = '<br><span style="color:' + this.series.color + '">' + this.series.name + '</span>: ' + this.series.data[0].y + ' out of ' + totalDays + ' days (' + percent + '%)';
+          return tooltip;
+        } 
+      }
+    });
+}
 
-  },
-  tooltip: {
-    formatter: function() {
-      var tooltip = '<br><span style="color:' + this.series.color + '">' + this.series.name + '</span>: ' + this.series.data[0].y + ' out of 300 days (50%)';
-      return tooltip;
-    } 
-  }
-});
-
-
-})
+function sleepTracker(sleepArray, totalDays){
+    Highcharts.chart('sleepTracker', {
+        credits: {
+            enabled: false
+          },
+          title:{
+              text: null
+          },
+        exporting: {
+            enabled: false
+        },
+        chart:{
+            type: 'column'
+        },
+        yAxis: {
+            title: {
+                text: 'Number of Days'
+            },
+            tickInterval: 1,
+            min: 0
+        },
+        xAxis: {
+           min: 0,
+           max: 24,
+           tickInterval: 1,
+           title: {
+            text: 'Hours Slept'
+            }
+        },
+    
+        legend: {
+            enabled: false
+        },
+    
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 0,
+            }
+        },
+    
+        series: [{
+            name: '',
+            data: sleepArray
+        }],
+        tooltip: {
+            formatter: function() {
+                return 'Slept for <strong>' + this.x + '</strong> hours in <strong>' + this.y + '</strong> day(s) out of ' + totalDays + ' days';
+            }
+        },
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    
+    });
+}
