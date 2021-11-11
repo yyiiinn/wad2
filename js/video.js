566d974a321d0d18908661e853fc0592f59ec453
huisinnnn <huisin.lim.2018@smu.edu.sg>
2021-11-12 02:21:39 +0800

    Added function to filter table by status

--- js/video.js
+++ js/video.js
@@ -39,3 +39,21 @@ async function getAllVideos(){
 };
 
 getAllVideos();
+
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("mylist");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("video-post");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
